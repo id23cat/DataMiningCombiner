aa3ff447f1a46e4389f5a1040463ed23f2e6a8f5
ID23cat <demidchuk.aleksey@gmail.com>
2017-03-03 18:48:33 +0300

    initial DMCombiner

---
+++ DataMiningCombiner/src/main/java/by/bsuir/evm/DataMiningCombinerApplication.java
@@ -0,0 +1,12 @@
+package by.bsuir.evm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMiningCombinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMiningCombinerApplication.class, args);
+	}
+}
