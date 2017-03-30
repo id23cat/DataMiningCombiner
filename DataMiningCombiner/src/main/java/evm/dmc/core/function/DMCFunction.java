4890ed1b78838b99c2bcd0fac9b5a8c2745e0ae2
ID23cat <demidchuk.aleksey@gmail.com>
2017-03-30 19:09:54 +0300

    @Scope("prototype") as default scope for all functions

--- DataMiningCombiner/src/main/java/evm/dmc/core/function/DMCFunction.java
+++ DataMiningCombiner/src/main/java/evm/dmc/core/function/DMCFunction.java
@@ -1,56 +1,64 @@
 package evm.dmc.core.function;
 
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Scope;
 
 import evm.dmc.core.FrameworkContext;
 import evm.dmc.core.data.Data;
 
+@Scope("prototype")
 public interface DMCFunction<T> {
 	void execute();
 
-	default String getName() {return "Warning: No name specified";}
+	default String getName() {
+		return "Warning: No name specified";
+	}
 
 	Integer getArgsCount();
-	
+
 	void setName(String name);
-	
+
 	void setArgsCount(Integer count);
-	
+
 	void addArgument(Data<T> arg);
-	
+
 	Data<T> getResult();
-	
-//	Data<T> getResult;
-	
+
+	// Data<T> getResult;
+
 	/**
-	 * Returns object that should be used in function object as context of execution
+	 * Returns object that should be used in function object as context of
+	 * execution
+	 * 
 	 * @return
 	 */
 	FrameworkContext getContext();
-	
+
 	/**
-	 * Sets object that should be used in function object as context of execution
+	 * Sets object that should be used in function object as context of
+	 * execution
+	 * 
 	 * @return
 	 */
 	void setContext(FrameworkContext context);
-	
+
 	/**
-	 * @author id23cat
-	 * Interface describes requirements to the context of execution for concrete function
+	 * @author id23cat Interface describes requirements to the context of
+	 *         execution for concrete function
 	 */
-//	@Component
-//	public interface FrameworkContext<T> {
-//		/**
-//		 * Method is used for first initialization of function context or resetting settings to default
-//		 */
-//		void initContext();		
-//		
-//		/**
-//		 * Execute concrete function in context
-//		 * @param function
-//		 */
-//		void executeFunction(DMCFunction<T> function);
-//
-//	}
+	// @Component
+	// public interface FrameworkContext<T> {
+	// /**
+	// * Method is used for first initialization of function context or
+	// resetting settings to default
+	// */
+	// void initContext();
+	//
+	// /**
+	// * Execute concrete function in context
+	// * @param function
+	// */
+	// void executeFunction(DMCFunction<T> function);
+	//
+	// }
 
 }
