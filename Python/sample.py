e61f329f1b3d3fdd7a664d3e2887516ddc943727
ID23cat <demidchuk.aleksey@gmail.com>
2017-04-04 17:21:00 +0300

    Main: sklearn.cross_validation (which is deperecated) has been changed by sklearn.model_selection; Minor: added ignoring of project files

--- Python/sample.py
+++ Python/sample.py
@@ -1,6 +1,7 @@
 import csv
 from sklearn import preprocessing
-from sklearn.cross_validation import train_test_split
+#from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 #from sklearn.pipeline import Pipeline
