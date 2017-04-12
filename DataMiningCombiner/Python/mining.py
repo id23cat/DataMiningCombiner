b443136e5a53e5dc58de29bd90b7267df0f29418
ID23cat <demidchuk.aleksey@gmail.com>
2017-04-12 18:01:47 +0300

    Two versions of normalization

--- DataMiningCombiner/Python/mining.py
+++ DataMiningCombiner/Python/mining.py
@@ -4,13 +4,13 @@ import sklearn as sk
 # noramlization of numeric columns in DafaFrame
 # data -- Pandas DataFrame
 # result -- numpy.ndarray
-def normalization.stdscore(data):
+def norm_stdscore(data):
     numerical_columns = [c for c in data.columns if data[c].dtype.name != 'object']
     data_numerical = data[numerical_columns]
     result=(data[numerical_columns] - data[numerical_columns].mean())/data[numerical_columns].std()
     return result
 
-def normalization.sklearn(data):
+def norm_sklearn(data):
     numerical_columns = [c for c in data.columns if data[c].dtype.name != 'object']
     data_numerical = data[numerical_columns]
     result=sklearn.preprocessing.normalize(data_numerical)
