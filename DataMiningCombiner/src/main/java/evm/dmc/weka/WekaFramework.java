b1762b03e00cf747e0d5f638a1f4a734790b9f77
ID23cat <demidchuk.aleksey@gmail.com>
2017-04-29 16:21:31 +0300

    Added method in Weka Framework for casting to own datatype

--- DataMiningCombiner/src/main/java/evm/dmc/weka/WekaFramework.java
+++ DataMiningCombiner/src/main/java/evm/dmc/weka/WekaFramework.java
@@ -3,7 +3,11 @@ package evm.dmc.weka;
 import org.springframework.stereotype.Service;
 
 import evm.dmc.core.AbstractFramework;
+import evm.dmc.core.data.Data;
+import evm.dmc.weka.data.WekaData;
 import evm.dmc.weka.function.AbstractWekaFunction;
+import evm.dmc.weka.function.ClusteringError;
+import weka.core.Instances;
 
 @Service
 @WekaFW
@@ -16,6 +20,18 @@ public class WekaFramework extends AbstractFramework {
 	@Override
 	public void initFramework() {
 		super.initFrameworkForType(AbstractWekaFunction.class);
+	}
+
+	public WekaData castToWekaData(Data data) throws ClusteringError {
+		WekaData wekaData = data instanceof WekaData ? (WekaData) data : null;
+		if (wekaData == null) {
+			Instances inst = data.getData() instanceof Instances ? (Instances) data.getData() : null;
+			if (inst == null)
+				throw new ClusteringError("Unsupported data type");
+			wekaData = (WekaData) super.getData(WekaData.class);
+			wekaData.setData(inst);
+		}
+		return wekaData;
 	}
 
 }
