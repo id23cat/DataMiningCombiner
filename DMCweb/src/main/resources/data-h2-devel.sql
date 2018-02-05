44cc0a942f728454db9a2a43168899b204a92b3c
admin <admin@DESKTOP-7GE6820>
2018-02-05 19:02:04 +0300

    Use user 'devel' instead 'idcat'

--- DMCweb/src/main/resources/data-h2-devel.sql
+++ DMCweb/src/main/resources/data-h2-devel.sql
@@ -1,14 +1,14 @@
 -- user: admin
 -- password: adminpass
 INSERT INTO ACCOUNT(password, user_name, email, first_Name, last_Name, role, created, dtype) 
-			VALUES ('cat42pass', 'idcat', 'idcat@mail.org', 'Alex', 'Dem', 'USER', CURRENT_TIMESTAMP(), 'Account');
+			VALUES ('devel', 'devel', 'devel@mail.org', 'Devel', 'Oper', 'USER', CURRENT_TIMESTAMP(), 'Account');
 
 INSERT INTO PROJECT(name, project_type, account_id, created)
-			VALUES ('proj0', 'SIMPLEST_PROJECT', (SELECT id FROM ACCOUNT WHERE user_name='idcat'), CURRENT_TIMESTAMP());
+			VALUES ('proj0', 'SIMPLEST_PROJECT', (SELECT id FROM ACCOUNT WHERE user_name='devel'), CURRENT_TIMESTAMP());
 INSERT INTO PROJECT(name, project_type, account_id, created)
-			VALUES ('proj1', 'SIMPLEST_PROJECT', (SELECT id FROM ACCOUNT WHERE user_name='idcat'), CURRENT_TIMESTAMP());
+			VALUES ('proj1', 'SIMPLEST_PROJECT', (SELECT id FROM ACCOUNT WHERE user_name='devel'), CURRENT_TIMESTAMP());
 INSERT INTO PROJECT(name, project_type, account_id, created)
-			VALUES ('proj2', 'SIMPLEST_PROJECT', (SELECT id FROM ACCOUNT WHERE user_name='idcat'), CURRENT_TIMESTAMP());
+			VALUES ('proj2', 'SIMPLEST_PROJECT', (SELECT id FROM ACCOUNT WHERE user_name='devel'), CURRENT_TIMESTAMP());
 			
 INSERT INTO ALGORITHM(name,	PARENT_PROJECT_ID)
 			VALUES('alg1', (SELECT id FROM PROJECT WHERE name='proj0'));
