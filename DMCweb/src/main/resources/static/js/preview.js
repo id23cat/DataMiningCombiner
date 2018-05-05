ff1e2d633c5463e7e5745b39291cd7051c086f48
Aleksey Demidchuk <demidchuk.aleksey@gmail.com>
2018-05-05 15:04:05 +0300

    Hiding elements on preview

--- DMCweb/src/main/resources/static/js/preview.js
+++ DMCweb/src/main/resources/static/js/preview.js
@@ -9,8 +9,31 @@ $("input[id^='chkbx_']").change(function() {
 	var idchk = $(this)[0].id;
 	var multiplier_element = "#" + idchk.replace("chkbx_", "mul_grp_");
 	var datatype_element = "#" + idchk.replace("chkbx_", "type_grp_");
+	var type = "#" + idchk.replace("chkbx_", "type_");
 
-	$(multiplier_element).prop('hidden', !$(this).prop("checked"));
-	$(datatype_element).prop('hidden', !$(this).prop("checked"));
+	setHidden($(multiplier_element), !$(this).prop("checked"));
+	setHidden($(datatype_element), !$(this).prop("checked"));
+	if($(this).prop("checked")) {
+		$(type).change();
+	}
 
 });
+
+
+$("select[id^='type_']").change(function() {
+	hiddenMultilier(this);
+})
+
+
+
+function hiddenMultilier(selector) {
+	var selectorId = $(selector)[0].id;
+	var multiplier_element = "#" + selectorId.replace("type_", "mul_grp_");
+	console.log("selsected: " + $(selector).val());
+	setHidden($(multiplier_element), !($(selector).val() == "NUMERIC"));
+}
+
+function setHidden(id, hidden) {
+	$(id).prop('hidden', hidden);
+}
+
