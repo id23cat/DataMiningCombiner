77d0310fe9291988921ddee962ce9286cd9e0404
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-11-18 21:27:21 +0300

    REST-1 - Update AbstractDto.java

--- DMC-webApi/src/main/java/evm/dmc/webApi/dto/AbstractDto.java
+++ DMC-webApi/src/main/java/evm/dmc/webApi/dto/AbstractDto.java
@@ -2,6 +2,10 @@ package evm.dmc.webApi.dto;
 
 import org.springframework.hateoas.ResourceSupport;
 
+/**
+ * Parent class for DTO provided by HATEOAS REST API
+ */
 public abstract class AbstractDto extends ResourceSupport {
+
 	public abstract Long getDtoId();
 }
